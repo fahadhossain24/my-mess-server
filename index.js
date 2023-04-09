9018094bf003116dfcdedb473334a9006d106d06
FAHAD-HOSSAIN-24 <fahadhossaim24@gmail.com>
2023-04-09 22:54:27 +0600

    working on admin card, meal cost, mess cost

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const  ObjectId = require('mongodb').ObjectId;
+const ObjectId = require('mongodb').ObjectId;
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -77,82 +77,104 @@ async function run() {
             if (existOwner) {
                 const expectedMess = await messCollection.findOne(query);
                 res.send(expectedMess);
-            }else{
-                res.send({message: 'Sorry! You have no mess. For access dashboard either you must have a mess or you have stay as mess member'})
+            } else {
+                res.send({ message: 'Sorry! You have no mess. For access dashboard either you must have a mess or you have stay as mess member' })
             }
 
         })
 
         //get mess information by id
-        app.get('/messById/:id', async(req, res) => {
+        app.get('/messById/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id:new ObjectId(id)};
-            const expectedMess = await messCollection.findOne(query);
-            res.send(expectedMess);
+            const query = {};
+            const cursor = messCollection.find(query);
+            const allMess = await cursor.toArray();
+            const filteredId = allMess.find(mess => { 
+                const messId = mess._id.toString();
+                const matchedMess = messId === id;
+                return matchedMess;
+            });
+            // console.log(filteredId)
+            if (filteredId) {
+                const query = {_id: new ObjectId(id)};
+                const expectedMess = await messCollection.findOne(query);
+                res.send(expectedMess);
+            }else{
+                res.send({message: 'Worng mess id!!!'});
+            }
+
         })
 
 
         //post or update new mess member information to database
-        app.put('/addMessMember/:email', async(req, res) => {
+        app.put('/addMessMember/:email', async (req, res) => {
             const memberEmail = req.params.email;
             const memberInfo = req.body;
             const query = {};
             const cursor = userCollection.find(query);
             const allUser = await cursor.toArray();
             const isAccountAxist = allUser.find(user => user.userEmail === memberEmail);
-            if(isAccountAxist){
-                const filter = {emailAddress: memberEmail};
-                const options = {upsert: true};
+            if (isAccountAxist) {
+                const filter = { emailAddress: memberEmail };
+                const options = { upsert: true };
                 const updateDoc = {
                     $set: memberInfo,
                 };
                 const result = await messMemberCollection.updateOne(filter, updateDoc, options);
                 res.send(result);
-            }else{
-                res.send({message: `Opps! ${memberInfo.emailAddress} can't find. This member haven\'t an account on this website`});
+            } else {
+                res.send({ message: `Opps! ${memberInfo.emailAddress} can't find. This member haven\'t an account on this website` });
             }
         })
 
-        //get all current member from messMember collection..........
-        app.get('/messMember', async(req, res) => {
+        //get all current mess member from messMember collection..........
+        app.get('/messMember', async (req, res) => {
             const query = {};
             const cursor = messMemberCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
         })
 
+        //get specific current mess member by email from messMember collection..........
+        app.get('/messMember/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { emailAddress: email };
+            const getSpecificMember = await messMemberCollection.findOne(query);
+            res.send(getSpecificMember);
+        })
+
         // post or update member request
-        app.put('/requestedMember/:email', async(req, res) => {
+        app.put('/requestedMember/:email', async (req, res) => {
             const email = req.params.email;
             const requestedMemberInfo = req.body;
-            const filter = {email: email};
-            const options = {upsert: true};
+            const filter = { email: email };
+            const options = { upsert: true };
             const updateDoc = {
                 $set: requestedMemberInfo,
             }
-            const result = await requestedMemberCollectionn.updateOne(filter, updateDoc,  options);
+            const result = await requestedMemberCollectionn.updateOne(filter, updateDoc, options);
             res.send(result);
         })
 
         //get all requested member .....
-        app.get('/requestedMember', async(req, res) => {
+        app.get('/requestedMember', async (req, res) => {
             const query = {};
             const cursor = requestedMemberCollectionn.find(query);
             const result = await cursor.toArray();
             res.send(result);
         })
         // delete indevisual requested member from database....
-        app.delete('/requestedMember/:email', async(req, res) => {
+        app.delete('/requestedMember/:email', async (req, res) => {
             const email = req.params.email;
-            const query = {email: email};
+            const query = { email: email };
             const result = await requestedMemberCollectionn.deleteOne(query);
             res.send(result);
         })
 
         //delete indevisual mess member from database....
-        app.delete('/messMember/:email', async(req, res) => {
+        app.delete('/messMember/:email', async (req, res) => {
             const email = req.params.email;
-            const query = {emailAddress: email};
+            const query = { emailAddress: email };
             const result = await messMemberCollection.deleteOne(query);
             res.send(result);
         })
